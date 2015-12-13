4816150933ceef4cab9127c73f97160163be2e6c
Blake Bertuccelli <blakebertuccelli@gmail.com>
2015-12-13 17:36:42 -0600

    Add USWDS javascript

--- functions.php
+++ functions.php
@@ -1,15 +1,16 @@
 <?php
 
-/**
- * Enqueue USWDS Theme Assets
- */
+//Enqueue USWDS Theme Assets
 function uswds_assets() {
   wp_enqueue_style('uswds-css', get_template_directory_uri() . '/assets/css/main.css');
   wp_enqueue_style('uswds-fonts', get_template_directory_uri() . '/assets/css/google-fonts.css');
-  wp_enqueue_script( 'jquery' );
-  wp_enqueue_script('sage/js', get_template_directory_uri() . '/assets/js/components.js', array('jquery'));
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('html5shiv', get_template_directory_uri() . '/assets/js/html5shiv.js', '', '3.7.1');
+  wp_enqueue_script('rem', get_template_directory_uri() . '/assets/js/rem.min.js', '', '1.3.2');
+  wp_enqueue_script('respond', get_template_directory_uri() . '/assets/js/rem.min.js', '', '1.4.0');
+  wp_enqueue_script('selectivizr', get_template_directory_uri() . '/assets/js/selectivizr.min.js', '', '1.0.3b');
+  wp_enqueue_script('uswds-components', get_template_directory_uri() . '/assets/js/components.js', array('jquery'));
 }
 add_action('wp_enqueue_scripts', 'uswds_assets');
 
-  
 ?>
