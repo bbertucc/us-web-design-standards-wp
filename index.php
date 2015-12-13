1d134abdd3383eb232c69f7c672e4fcbfd8f893a
Blake Bertuccelli <blakebertuccelli@gmail.com>
2015-12-13 17:37:26 -0600

    The most basic WP template.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <!-- 
+      
+      Where are the elements? Coming soon.. 
+      
+      For now, add anything! And share your ideas!  
+      
+      Remember: Style will only show if you keep to the Standards: https://playbook.cio.gov/designstandards/ 
+      
+    -->
+    
+    <?php wp_footer(); ?>
+  </body>
+</html>
